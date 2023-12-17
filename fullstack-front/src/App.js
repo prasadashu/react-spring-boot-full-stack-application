c2cb6573be160d610c0ff67a9d8ba1a12b487af5
Ashu Prasad <ashuprasad29516@gmail.com>
2023-12-17 00:18:17 -0600

    feat: added routes for components

--- fullstack-front/src/App.js
+++ fullstack-front/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './layout/Navbar';
+import Home from './pages/Home';
+import AddUser from './users/AddUser';
 
 function App() {
+  /*
+    Function to render Main Application page
+   */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        {/* Adding Navbar Component */}
+        <Navbar/>
+
+        {/* Adding Routes */}
+        <Routes>
+          {/* Adding route endpoint for Home Component */}
+          <Route exact path="/" element={<Home/>}/>
+          {/* Adding route endpoint for AddUser Component */}
+          <Route exact path="/adduser" element={<AddUser/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
