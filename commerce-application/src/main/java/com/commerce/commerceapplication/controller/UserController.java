e7800bc70cf35a167e01a4a55b730224cb00f40a
Ashu Prasad <ashuprasad29516@gmail.com>
2023-12-17 00:19:37 -0600

    feat: added cross origin annotation

--- commerce-application/src/main/java/com/commerce/commerceapplication/controller/UserController.java
+++ commerce-application/src/main/java/com/commerce/commerceapplication/controller/UserController.java
@@ -2,15 +2,13 @@ package com.commerce.commerceapplication.controller;
 
 import com.commerce.commerceapplication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.commerce.commerceapplication.model.User;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class UserController {
 
     // Inject UserRepository to UserController class
