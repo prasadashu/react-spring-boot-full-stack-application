57cc52680ebef50b6771ad80e9cde826cc6194ef
Ashu Prasad <ashuprasad29516@gmail.com>
2023-12-17 20:50:28 -0600

    feat: added feature to redirect users to home page

--- fullstack-front/src/layout/Navbar.js
+++ fullstack-front/src/layout/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
                 {/* Define navigation bar */}
-                <a className="navbar-brand" href="#">Full Stack Application</a>
+                <Link className="navbar-brand" to={"/"}>Full Stack Application</Link>
                 <button className="navbar-toggler" 
                         type="button" data-bs-toggle="collapse" 
                         data-bs-target="#navbarSupportedContent" 
